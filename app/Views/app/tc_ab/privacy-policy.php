4f3708639cc96896b5782afa3e6172835dba5e66
karthi2203 <karthikshettigar681@gmail.com>
2024-01-28 20:28:19 +0530

    privacy policy update

--- app/Views/app/tc_ab/privacy-policy.php
+++ app/Views/app/tc_ab/privacy-policy.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Policya and Privacy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="policy.css">
+    <link rel="stylesheet" href="<?= base_url('assets/web/css/policy.css') ?>">
 </head>
-<body
+<body>
     <div class="d-flex justify-content-center main" >
         <div class="content mt-5  text-light">
             <h1 class="h2 mt-1 heading">Privacy and Policy</h1>
