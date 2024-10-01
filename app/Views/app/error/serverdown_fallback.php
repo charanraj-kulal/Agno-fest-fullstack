88d487ea268dde523e146ff7be13f2e9d05e4df6
charanraj-kulal <charanraj9731@gmail.com>
2024-10-01 20:38:25 +0530

    tech issue

---
+++ app/Views/app/error/serverdown_fallback.php
@@ -0,0 +1,28 @@
+    <div class="container-fluid py-5 bg-dark hero-header mb-5">
+        <section id="home">   
+            <div class="container my-5 py-5">
+                <div class="row  g-5">
+                    <div class="col-lg-6 text-center text-lg-start">
+                       
+                        
+                        <p class="motivation-sub text-white  animated slideInLeft mb-5 pb-2">We're experiencing a technical issue. Please try again later.</p>
+                       
+                        
+
+                        </div>
+                        <div class="col-lg-6 text-center text-lg-end ">
+                            <img class="img-fluid mandal-1 d-none d-sm-none d-md-block" src="<?= base_url('assets/web/imgs/BADGE.png') ?>" alt="">
+                        
+                   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+        
+      
+       
+
+
+       
